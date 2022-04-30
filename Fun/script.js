4f5d507ed3eac363fa28ce16798d55a8ab4a9577
Sebastian Czempik <sebochrisczemp@gmail.com>
2022-04-30 18:29:34 +0200

    Update script.js

--- Fun/script.js
+++ Fun/script.js
@@ -1,7 +1,23 @@
+var Clicked = 0
+
 function FunInputLink() {
     prompt("FUN INPUT!")
 }
 
 function FunLink() {
-    alert("FUN LINK!")
+    Clicked = Clicked + 1
+    if (Clicked == 10) {
+        while (true) {
+            var PromtInput = prompt("DO YOU HAVE FUN? YES OR NO")
+            if (PromtInput === "YES") {
+                alert("FUN!")
+            } else if (PromtInput === "NO") {
+                alert("WHY?")
+            } else {
+                alert("YES OR NO")
+            }
+        }
+    } else {
+        alert("FUN LINK!")
+    }
 }
